fd18448ffe2f3e2f38844a99a0abeca8b805833e
Lucy Stringer <lucyjstringer@gmail.com>
2021-02-04 17:03:49 +0000

    added get_first_letter_and_length method

--- run_length_encoding/lib/encoder.rb
+++ run_length_encoding/lib/encoder.rb
@@ -6,4 +6,10 @@ class Encoder
     def get_first_letter(string)
         string[0]
     end
+
+    def get_first_letter_and_length(string)
+        length = string.length.to_s
+        first_letter = string[0]
+        return  first_letter + length
+    end
 end
